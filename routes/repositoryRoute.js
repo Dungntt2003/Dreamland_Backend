df85bf9c8df30cceb9660687f751b45428fe8a2c
Dungntt2003 <nguyenthuydung55555@gmail.com>
2025-02-23 21:45:18 +0700

    Update api with dulicate api route

--- routes/repositoryRoute.js
+++ routes/repositoryRoute.js
@@ -10,7 +10,7 @@ const {
 
 route.get("/:userId", authenticateToken, getAll);
 route.post("/", authenticateToken, createNew);
-route.get("/:id", authenticateToken, getFullDemo);
+route.get("/detail/:id", authenticateToken, getFullDemo);
 route.put("/updateDescription/:id", authenticateToken, updateRepoWithDes);
 
 module.exports = route;
