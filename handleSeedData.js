7fccddf07742a5fef345757cfd697940bc32561c
Dungntt2003 <nguyenthuydung55555@gmail.com>
2025-06-14 00:59:38 +0700

    Config database

--- handleSeedData.js
+++ handleSeedData.js
@@ -1,17 +1,23 @@
 const db = require("./models/index");
 const updateCoordinatesForModel = require("./utils/getLat");
-const updateRatingForModel = require("./utils/getRating")(async () => {
+const updateRatingForModel = require("./utils/getRating");
+
+async function main() {
   try {
     await updateRatingForModel(db.Sight);
     await updateRatingForModel(db.Restaurant);
     await updateRatingForModel(db.Hotel);
     await updateRatingForModel(db.Entertainment);
+
     await updateCoordinatesForModel(db.Sight);
     await updateCoordinatesForModel(db.Restaurant);
     await updateCoordinatesForModel(db.Hotel);
     await updateCoordinatesForModel(db.Entertainment);
-    console.log("Done updating ratings!");
+
+    console.log("Done updating ratings and coordinates!");
   } catch (error) {
-    console.error("Error updating: ", error);
+    console.error("Error updating data:", error);
   }
-})();
+}
+
+main();
