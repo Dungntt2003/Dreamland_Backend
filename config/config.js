5fe0f099e4306bcc29598442d75b9ff4bd57dcac
Dungntt2003 <nguyenthuydung55555@gmail.com>
2025-06-14 00:42:21 +0700

    Add config to deploy

--- config/config.js
+++ config/config.js
@@ -3,23 +3,26 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: "database_test",
     host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: "DATABASE_URL",
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
