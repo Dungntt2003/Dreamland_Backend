a209b896ce58ba4c0d08cb23922a9269771f7f07
Dungntt2003 <nguyenthuydung55555@gmail.com>
2025-06-14 18:10:27 +0700

    Add env arg in llm

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -40,7 +40,9 @@ const integrateLLM = async (req, res) => {
   try {
     console.log("Request body:", req.body);
     const response = await axios.post(
-      "http://127.0.0.1:5000/api/generate-description",
+      `${
+        process.env.API_BASE_URL || "http://127.0.0.1:5000"
+      }/api/generate-description`,
       req.body
     );
     res.status(200).json({
